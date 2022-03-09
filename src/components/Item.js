08eec3b67dd02e6c1fb53ec31f93e4e8029c902d
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2022-03-09 14:20:33 -0600

    commit tree f07539dbb7981411aa698c1f999d8e8f1ac2101d

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [inCart, setCart] = useState(false);
+  const cart = inCart ? "in-cart" : "";
+
+  function handleCart(){
+    setCart(inCart => !inCart)
+  }
+
   return (
-    <li className="">
+    <li className={cart}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleCart}>{inCart ? "Remove From Cart" : "Add to Cart"}</button>
     </li>
   );
 }
